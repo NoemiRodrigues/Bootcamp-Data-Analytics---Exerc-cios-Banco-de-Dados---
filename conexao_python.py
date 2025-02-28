b0791b57fc796bfc12e572e51229357807c2a328
Noemi Rodrigues de Almeida <148573627+NoemiRodrigues@users.noreply.github.com>
2025-02-28 12:51:36 -0300

    Update conexao_python.py
    retirando comentários

--- conexao_python.py
+++ conexao_python.py
@@ -3,7 +3,7 @@ import sqlite3
 conexao = sqlite3.connect ('semana_do_sql')
 
 cursor = conexao.cursor()
-#cursor.execute('CREATE TABLE alunos (id INT, nome VARCHAR(100), idade INT, curso VARCHAR (100))')
+cursor.execute('CREATE TABLE alunos (id INT, nome VARCHAR(100), idade INT, curso VARCHAR (100))')
 cursor.execute('INSERT INTO alunos (id, nome, idade, curso) VALUES (1, "Laura", 28, "engenharia")')
 cursor.execute('INSERT INTO alunos (id, nome, idade, curso) VALUES (2, "Miguel", 15, "física")')
 cursor.execute('INSERT INTO alunos (id, nome, idade, curso) VALUES (3, "Geovana", 20, "física nuclear")')
