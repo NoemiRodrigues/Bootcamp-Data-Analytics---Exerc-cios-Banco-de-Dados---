d862232ea51f30c629b16b0f0f1ad551dcbe7a98
Noemi Rodrigues de Almeida <148573627+NoemiRodrigues@users.noreply.github.com>
2025-02-28 12:52:37 -0300

    Update questao5.py
    retirando comentários

--- questao5.py
+++ questao5.py
@@ -3,7 +3,7 @@ import sqlite3
 conexao = sqlite3.connect ('semana_do_sql')
 
 cursor = conexao.cursor()
-#cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY, nome VARCHAR(100), idade INT, saldo REAL)')
+cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY, nome VARCHAR(100), idade INT, saldo REAL)')
 cursor.execute('INSERT INTO clientes (id, nome, idade, saldo) VALUES (1, "Geovana", 28, 158.9)')
 cursor.execute('INSERT INTO clientes (id, nome, idade, saldo) VALUES (2, "Felipinho", 50, 999.60)')
 cursor.execute('INSERT INTO clientes (id, nome, idade, saldo) VALUES (3, "Ana Luiza", 20, 650)')
